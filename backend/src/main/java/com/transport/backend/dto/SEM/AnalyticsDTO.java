3e6cd27924f53badb1566d044edccac58a9c6a07
vyvy0808 <dolekhanhvy0808@gmail.com>
2026-05-02 01:28:13 +0700

    SEM + Docker

---
+++ backend/src/main/java/com/transport/backend/dto/SEM/AnalyticsDTO.java
@@ -0,0 +1,9 @@
+package com.transport.backend.dto.SEM;
+
+public class AnalyticsDTO {
+    public double fleetSize;
+    public double capacity;
+    public double ontimeRate;
+    public double fuelEfficiency;
+    public double profit;
+}
